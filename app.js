418148a974dfde2155a2349ab4ce76d771778b1a
lachlan <lachlan.miller.1990@outlook.com>
2017-01-14 16:35:34 +0900

    Add express app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path      = require('path')
 const env       = process.env.NODE_ENV || 'development'
 const config    = require(__dirname + '/config/config.json')[env]
 const utils     = require(__dirname + '/utils/databaseUtils.js')
+const express   = require('express')
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
@@ -13,5 +14,15 @@ utils.authenticate(sequelize)
 
 models.posts.findAll()
   .then = posts => console.log('Selected all posts:', posts)
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World.')
+})
+
+app.listen(3000, () => {
+  console.log('App started on port 3000.')
+})
 
 
