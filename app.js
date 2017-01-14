30b5842980524eb4fdcf45fb0588a3730cf37d83
lachlan <lachlan.miller.1990@outlook.com>
2017-01-14 16:22:36 +0900

    Remove unnecesssary requires

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-const mysql     = require('mysql2')
 const models    = require('./models')
 const Sequelize = require('sequelize')
 const path      = require('path')
-const basename  = path.basename(module.filename)
 const env       = process.env.NODE_ENV || 'development'
 const config    = require(__dirname + '/config/config.json')[env]
 const utils     = require(__dirname + '/utils/databaseUtils.js')
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-models.sequelize.sync()
-
-sequelize.authenticate()
-  .then((err)  => console.log('Sucessfully connected.')) 
-  .catch((err) => console.log('Error:', err))
+//models.sequelize.sync()
 
 utils.authenticate(sequelize)
 
