0a10f06dfaa2b69576450713f5bef9ffd2f37042
Lachlan <lachlan.miller.1990@outlook.com>
2017-01-19 13:57:03 +0000

    Allow CORS

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 
 const app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', index)
 app.use('/posts', posts)
